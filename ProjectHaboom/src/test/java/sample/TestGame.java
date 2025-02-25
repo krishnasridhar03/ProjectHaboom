4da7048d8de1e1bed5bafd3635d6660757802b78
Krishna Sridhar <Krishna Sridhar@Testing-Sys372>
2025-02-25 10:32:23 +0530

    test 25/2/25

---
+++ ProjectHaboom/src/test/java/sample/TestGame.java
@@ -0,0 +1,49 @@
+package sample;
+
+import java.time.Duration;
+import java.util.Iterator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestGame {
+	
+	WebDriver driver;
+	
+	
+	public static void main(String[]args) {
+		TestGame play=new TestGame();
+		play.setup();
+		play.login();
+		play.slotsdashboard();
+	}
+	
+     
+	public void setup() {
+    	WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://haboom.com/d/login");	
+		driver.manage().window().maximize();
+	}
+	
+	public void login() {
+		driver.findElement(By.id("email__")).sendKeys("krishnacri");
+		driver.findElement(By.id("password__")).sendKeys("Krishna@123");
+		driver.findElement(By.id("login")).click();
+	}
+	public void slotsdashboard() {
+		driver.findElement(By.id("ARCADE_SLOTS")).click();
+		driver.findElement(By.xpath("//*[@id=\'root\']/div/div[4]/div[2]/ul/li[9]/label/input")).click();
+		driver.findElement(By.xpath("//*[@id=\'display_providers_list\']/div[1]/div/section[1]")).click();
+	}
+	public void gamepage() {
+		for (String handle:driver.getWindowHandles()) {
+			driver.switchTo().window(handle);
+		}
+		driver.findElement(By.id("gameCanvas"))
+	}
+}
+	
